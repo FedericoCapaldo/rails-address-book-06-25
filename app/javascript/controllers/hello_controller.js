b3cdd296a378af47a00fbf0eee4a25645b3dcbf6
Federico Capaldo <federico.capaldo@hotmail.com>
2025-06-04 09:54:34 +0100

    post lecture

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    this.someFunction()
+  }
+
+
+
+  someFunction() {
+
+    function myCallback(data123) {
+      console.log("2. ", data123)
+    }
+
+    console.log("1. start")
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => console.log("2. ", data))
+      .catch(error => console.error('Error:', error));
+
+    console.log("3. finish")
+
   }
 }
